1840e47bdd730ec77a4460352f4faf53066054c3
orodev <oro91dev@gmail.com>
2018-05-23 19:52:22 +0200

    added more code
    app is fully working, some more things left to do

--- app.js
+++ app.js
@@ -1,20 +1,65 @@
 const form = document.getElementById('registrar');
 const input = form.querySelector('input');
+
+const mainDiv = document.querySelector('.main');
 const ul = document.getElementById('invitedList');
 
-form.addEventListener('submit', (e) => {
-  e.preventDefault();
-  const text = input.value;
-  input.value = ''; 
+const div = document.createElement('div');
+const filterLabel = document.createElement('label');
+const filerCheckBox = document.createElement('input');
+
+filterLabel.textContent = "Hide those who haven't responed";
+filerCheckBox.type = 'checkbox';
+div.appendChild(filterLabel);
+div.appendChild(filerCheckBox);
+mainDiv.insertBefore(div, ul);
+filerCheckBox.addEventListener('change', (e) => {
+    const isChecked = e.target.checked;
+    const lis = ul.children;
+    if(isChecked) {
+       for (let i = 0; i < lis.length; i+= 1){
+           let li = lis[i];
+           if (li.className === 'responded') {
+               li.style.display = '';
+           } else {
+               li.style.display = 'none'
+           }
+       }
+    } else{
+       for (let i = 0; i < lis.length; i+= 1){
+           let li = lis[i];
+           li.style.display = '';
+       }
+    }
+});
+
+function createLI(text) {
   const li = document.createElement('li');
-  ul.appendChild(li);
-  li.textContent = text;
+  const span = document.createElement('span');
+  span.textContent = text;
+  li.appendChild(span);    
   const label = document.createElement('label');
   label.textContent = 'Confirmed';
   const checkbox = document.createElement('input');
   checkbox.type = 'checkbox';
   label.appendChild(checkbox);
   li.appendChild(label);
+    
+  const editButton = document.createElement('button');
+  editButton.textContent = 'edit';
+  li.appendChild(editButton);  
+    
+  const removeButton = document.createElement('button');
+  removeButton.textContent = 'remove';
+  li.appendChild(removeButton);    
+  return li;
+}
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const text = input.value;
+  input.value = ''; 
+  const li = createLI(text);
   ul.appendChild(li);
 });
 
@@ -27,5 +72,35 @@ ul.addEventListener('change', (e) => {
       listItem.className = 'responded';    
     } else {
         listItem.className = '';
+    }
+});
+
+ul.addEventListener('click', (e) =>{
+    if (e.target.tagName === 'BUTTON'){
+        const button = e.target; 
+        const li = button.parentNode;
+        const ul = li.parentNode;
+        
+        if(button.textContent === 'remove'){
+        ul.removeChild(li);
+       }
+    else if(button.textContent === 'edit') {
+        const span = li.firstElementChild;
+        const input = document.createElement('input');
+        input.type = 'text';
+        input.value = span.textContent;
+        li.insertBefore(input, span);
+        li.removeChild(span);
+        button.textContent = 'save';
+    }
+    else if(button.textContent === 'save') {
+        const input = li.firstElementChild;
+        const span = document.createElement('span');
+        span.textContent = input.value;
+        li.insertBefore(span, input);
+        li.removeChild(input);
+        button.textContent = 'edit';
+    }
+        
     }
 });
